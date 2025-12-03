7b80e5df97636b22e04abfb46f1771932eda6464
gillrei <55304466+gillrei@users.noreply.github.com>
2025-12-02 23:02:59 -0300

    actualizacion de logica

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
 print("hola desde mi primer repo")
+
+
+nombre = input("¿Cómo te llamas? ")
+año_nacimiento = int(input("¿En qué año naciste? "))
+año_actual = 2025
+edad = año_actual - año_nacimiento
+print("Hola", nombre)
+print("Tienes", edad, "años.")
